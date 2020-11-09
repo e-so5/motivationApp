f32ae1d701b30a3c219f259bbfbb117e7ea871e1
Hiroyuki Tada <th1053nk@gmail.com>
2020-11-09 16:46:59 +0900

    create add_flask

--- app.py
+++ app.py
@@ -4,18 +4,7 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = "panda"
 
-# @app.route("/test")
-# def debug():
-#   return render_template("/tasklist.html")
-
-# @app.route("/test3")
-# def debug3():
-#   return render_template("/uselist.html")
-
-# @app.route("/resultwin")
-# def debug2():
-#   return render_template("/resultwin.html")
-
+# tasklistの作成
 @app.route("/tasklist")
 def task_list():
 	if "user_id" in session:
@@ -32,6 +21,28 @@ def task_list():
 	else:
 		return redirect("/login")
 
+# task追加のget通信
+	@app.route("/addtask")
+	def add_get():
+		if "user_id" in session:
+		return render_template("addpage.html")
+	else:
+			return redirect("/login")
+
+# task追加のpost通信
+	@app.route("/addtask",methods=["POST"])
+	def add_post():
+		user_id = session["user_id"][0]
+		add_task = request.form.get("task")
+		add_point = request.form.get("point")
+		conn = sqlite3.connect("flasktest.db")
+		c = conn.cursor()
+		c.execute("INSERT INTO tasktable VALUES(null,?,?,?)",(add_task, add_point,   user_id))
+		conn.commit()
+		c.close()
+		return redirect("/list")
+
+# uselistの作成
 @app.route("/uselist")
 def task_list():
 	if "user_id" in session:
@@ -47,6 +58,27 @@ def task_list():
 		return render_template("uselist.html",html_item_list = item_list)
 	else:
 		return redirect("/login")
+
+# uselit追加のget通信
+	@app.route("/adduselist")
+	def add_get():
+		if "user_id" in session:
+		return render_template("usepage.html")
+	else:
+			return redirect("/login")
+
+# uselist追加のpost通信
+	@app.route("/adduselist",methods=["POST"])
+	def add_post():
+		user_id = session["user_id"][0]
+		add_item = request.form.get("item")
+		add_point = request.form.get("point")
+		conn = sqlite3.connect("flasktest.db")
+		c = conn.cursor()
+		c.execute("INSERT INTO tasktable VALUES(null,?,?,?)",(add_item, add_point,   user_id))
+		conn.commit()
+		c.close()
+		return redirect("/list")
 
 if __name__ == "__main__":
 	app.run(debug=True)
